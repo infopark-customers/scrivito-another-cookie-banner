ea6fc309443016f7bc21583913165497bca89159
Anton Mezin <anton.mezin@infopark.de>
2022-10-07 23:29:14 +0200

    build done

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,18 @@
 import * as React from "react";
 import { Modal } from "react-bootstrap";
-import defaultLogo from "../assets/bima_logo_original.png";
 import { useCookieConsent } from "./CookieConsentContext";
 
 export default function Header() {
   const { isExtendedMode, logoUrl, I18n } = useCookieConsent();
   return (
     <Modal.Header closeButton={isExtendedMode()}>
-      <img
-        src={logoUrl || defaultLogo}
-        alt={I18n.t("BannerHeader.logoAlt", { ns: "cookieBanner" })}
-        className="img-fluid"
-      />
+      {logoUrl && 
+        <img
+          src={logoUrl}
+          alt={I18n.t("BannerHeader.logoAlt", { ns: "cookieBanner" })}
+          className="img-fluid"
+        />
+      }
     </Modal.Header>
   );
 }
