5f047b039b20c220df1c371d89b752695934ff09
natalka <natalia.tyszkiewicz@infopark.de>
2023-06-20 11:12:16 +0200

    Add keyboard navigation in footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Modal, Button } from "react-bootstrap";
 import { useCookieConsent } from "./CookieConsentContext";
+import { onKeyAccess } from "../utils/accessibilityHelper";
 
 export default function Footer() {
   const {
@@ -16,14 +17,16 @@ export default function Footer() {
       <Modal.Footer>
         <Button
           variant="primary"
-          onClick={() => saveAndClose()}
+          onClick={saveAndClose}
+          onKeyDown={(keyEvent) => onKeyAccess(keyEvent, () => saveAndClose())}
           className="btn-cta"
         >
           {I18n.t("buttons.saveAndClose", { ns: "cookieBanner" })}
         </Button>
         <Button
           variant="primary"
-          onClick={() => acceptAll()}
+          onClick={acceptAll}
+          onKeyDown={(keyEvent) => onKeyAccess(keyEvent, () => acceptAll())}
           className="btn-cta"
         >
           {I18n.t("buttons.acceptAll", { ns: "cookieBanner" })}
@@ -33,15 +36,30 @@ export default function Footer() {
   }
   return (
     <Modal.Footer>
-      <a role="button" onClick={() => switchBannerMode()} href="#bottom">
+      <a // TODO: can we make it Button? with this <a> element styles? or <div?
+        // Broken same-page link
+        // A link to another location within the page is present but does not have a corresponding target.
+        role="button"
+        onClick={switchBannerMode}
+        onKeyDown={(keyEvent) =>
+          onKeyAccess(keyEvent, () => switchBannerMode())
+        }
+        href="#bottom"
+      >
         {I18n.t("buttons.settings", { ns: "cookieBanner" })}
       </a>
-      <Button variant="primary" onClick={() => acceptAll()} className="btn-cta">
+      <Button
+        variant="primary"
+        onClick={acceptAll}
+        onKeyDown={(keyEvent) => onKeyAccess(keyEvent, () => acceptAll())}
+        className="btn-cta"
+      >
         {I18n.t("buttons.acceptAll", { ns: "cookieBanner" })}
       </Button>
       <Button
         variant="primary"
-        onClick={() => declineAll()}
+        onClick={declineAll}
+        onKeyDown={(keyEvent) => onKeyAccess(keyEvent, () => declineAll())}
         className="btn-cta"
       >
         {I18n.t("buttons.declineAll", { ns: "cookieBanner" })}
